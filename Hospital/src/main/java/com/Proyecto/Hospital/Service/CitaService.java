272e2351cac0f177041d10d4b0a5daab9bafe5f3
matthew bermudez <bequimatt.2005@gmail.com>
2026-08-02 14:12:39 -0600

    Implementar logica de reserva de citas y controlador de citas

--- Hospital/src/main/java/com/Proyecto/Hospital/Service/CitaService.java
+++ Hospital/src/main/java/com/Proyecto/Hospital/Service/CitaService.java
@@ -1,15 +1,27 @@
 package com.Proyecto.Hospital.Service;
 
 import com.Proyecto.Hospital.Model.Cita;
+import com.Proyecto.Hospital.Model.Medico;
+import com.Proyecto.Hospital.Model.Usuario;
 import com.Proyecto.Hospital.Repository.CitaRepository;
+import com.Proyecto.Hospital.Repository.MedicoRepository;
+import java.util.Optional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.time.LocalTime;
 
 @Service
 public class CitaService {
     private final CitaRepository citaRepository;
-    public CitaService(CitaRepository citaRepository) {
+    private final MedicoRepository medicoRepository;
+    
+    public CitaService(CitaRepository citaRepository, MedicoRepository medicoRepository) {
         this.citaRepository = citaRepository;
+        this.medicoRepository = medicoRepository;
     }
 
     public List<Cita> ListarPorUsuario(Long usuarioId) {
@@ -20,7 +32,70 @@ public class CitaService {
         return citaRepository.findAll();
     }
 
-    
+    private boolean horaEstaDisponible(String disponibilidadHorario , LocalTime hora) {
+        String[] horas = disponibilidadHorario.split(",");
+        for (String horaStr : horas) {
+            String[] rango = horaStr.trim().split("-");
+            LocalTime inicio = LocalTime.parse(rango[0].trim());
+            LocalTime fin = LocalTime.parse(rango[1].trim());
+            if (!hora.isBefore(inicio) && hora.isBefore(fin)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Transactional
+    public String Reservar (Usuario usuario, Long medicoId, LocalDate fecha, LocalTime hora) {
+        //Validr caampos obligatorios 
+        if (  medicoId == null || fecha == null || hora == null) {
+            return "Error: faltan campos obligatorios";
+        }
+
+        //Validar que el medico exista
+        Medico medicoExistente = medicoRepository.findById(medicoId).orElse(null);
+        if (medicoExistente == null) {
+            return "Error: medico no existe";
+        }
+
+        //No crear citas en pasado 
+        LocalDateTime fechaHoraCita = LocalDateTime.of(fecha, hora);
+        if (fechaHoraCita.isBefore(LocalDateTime.now())) {
+            return "Error: no se puede reservar citas en pasado";
+        }
+        
+        //Validar disponibilidad de horario
+        if (!horaEstaDisponible(medicoExistente.getDisponibilidadHoraria(), hora)) {
+            return "Error: el horario no está disponible para el médico seleccionado";
+        }
+
+        //Verificar espacio no ocupado
+        Optional<Cita> Cita = citaRepository.findByMedicoIdAndFechaAndHoraAndEstadoNot(medicoId, fecha, hora, "CANCELADA");
+        if (Cita.isPresent()) {
+            return "Error: el espacio ya está ocupado";
+        }
+
+        //Verificar que los usarios no tengan citas a la misma hora 
+        List<Cita> citas = citaRepository.findByUsuarioId(usuario.getId());
+        for (Cita c : citas) {
+            if (c.getFecha().equals(fecha) && c.getHora().equals(hora) && !c.getEstado().equals("CANCELADA")) {
+                return "Error: ya existe una cita en la misma hora";
+            }
+        }
+
+        // Crear cita
+        Cita cita = new Cita();
+        cita.setUsuario(usuario);
+        cita.setMedico(medicoExistente);
+        cita.setFecha(fecha);
+        cita.setHora(hora);
+        cita.setEstado("PENDIENTE");
+        citaRepository.save(cita);
+        return "Cita Reservada";
+    }
+
+
+}
 
         
-}
+
--- Hospital/src/main/java/com/Proyecto/Hospital/controller/CitaController.java
+++ Hospital/src/main/java/com/Proyecto/Hospital/controller/CitaController.java
@@ -2,12 +2,65 @@ package com.Proyecto.Hospital.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import com.Proyecto.Hospital.Service.CitaService;
+import com.Proyecto.Hospital.Service.MedicoService;
+import com.Proyecto.Hospital.Repository.UsuarioRepository;
+import com.Proyecto.Hospital.Model.Usuario;
+import org.springframework.ui.Model;
+import java.security.Principal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.time.LocalDate;
+import java.time.LocalTime;
 @Controller
 public class CitaController {
 
+    private final CitaService citaService;
+    private final MedicoService medicoService;
+    private final UsuarioRepository usuarioRepository;
+
+    public CitaController(CitaService citaService, MedicoService medicoService, UsuarioRepository usuarioRepository) {
+        this.citaService = citaService;
+        this.medicoService = medicoService;
+        this.usuarioRepository = usuarioRepository;
+    }
     @GetMapping("/citas")
-    public String citas() {
+    public String listarCitas(Model model, Principal principal) {
+        Usuario usuario = usuarioRepository.findByEmail(principal.getName()).orElse(null);
+        if (usuario == null) {
+            return "redirect:/login";
+        }
+        model.addAttribute("citas", citaService.ListarPorUsuario(usuario.getId()));
         return "citas";
     }
+
+    @GetMapping("/citas/nueva")
+    public String nuevaCita(Model model){
+        model.addAttribute("medicos", medicoService.ListarMedicos());
+        return "formularioCita";
+    }
+
+    @PostMapping("/citas/reservar")
+    public String reservarCita(@RequestParam Long medicoId,
+                                @RequestParam String fechaStr,
+                                @RequestParam String horaStr,
+                                Model model, Principal principal) {
+
+        Usuario usuario = usuarioRepository.findByEmail(principal.getName()).orElse(null);
+        if (usuario == null) {
+            return "redirect:/login";
+        }
+        String mensaje = citaService.Reservar(usuario, medicoId, LocalDate.parse(fechaStr), LocalTime.parse(horaStr));
+
+        if(mensaje.equals("Cita Reservada")) {
+            return "redirect:/citas";
+        } else {
+            model.addAttribute("error", mensaje);
+            model.addAttribute("medicos", medicoService.ListarMedicos());
+            return "formularioCita";
+
+        }
+
+    }
 }
+
